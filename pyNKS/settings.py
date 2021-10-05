501a043516dd9ef2283218ed874d792d82c89eeb
Srinadh Kotha <srinadhkotha8@gmail.com>
2021-10-05 11:53:31 +0530

    Use sqlite instead

--- pyNKS/settings.py
+++ pyNKS/settings.py
@@ -82,15 +82,8 @@ WSGI_APPLICATION = 'pyNKS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projectsuccess',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'sussilassi',
-        'OPTIONS':{
-            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
